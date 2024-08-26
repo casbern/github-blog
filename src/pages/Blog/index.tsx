826e3fc407ff849127e2f5af9e05e5dd71c8f07e
Cássia <cascrisbern@gmail.com>
2024-08-26 20:49:29 +0300

    added axios request for search issues on github repo

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -10,62 +10,156 @@ import {
 import { api } from '../../lib/axios'
 import { useEffect, useState } from 'react'
 
+interface UserDataProps {
+  avatar_url: string
+  name: string
+  bio: string
+  login: string
+  followers: number
+  company: string
+}
+
+interface SearchResultProps {
+  id: number
+  title: string
+  body: string
+  created_at: string
+}
+
 export function Blog() {
-  const [userData, setUserData] = useState('')
+  const [userData, setUserData] = useState<UserDataProps | null>(null)
+  const [searchIssues, setSearchIssues] = useState('')
+  const [searchResult, setSearchResult] = useState<SearchResultProps>()
+
+  console.log(searchResult)
+
+  function handleSearchIssues(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchIssues(event.target.value)
+  }
 
   async function getUserData() {
     try {
       const response = await api.get('/users/casbern')
       setUserData(response.data)
     } catch (error) {
-      console.error('Erro ao fazer requisição:', error)
+      console.error(error)
+    }
+  }
+
+  async function getSearch() {
+    if (!searchIssues) {
+      return
+    }
+
+    try {
+      const response = await api.get('/search/issues', {
+        params: {
+          q: `${searchIssues} repo:casbern/github-blog`,
+        },
+      })
+      console.log(response.data.items)
+      setSearchResult(response.data.items)
+    } catch (error) {
+      console.error(error)
     }
   }
 
   useEffect(() => {
     getUserData()
-  })
+  }, [])
 
-  console.log(userData)
+  useEffect(() => {
+    getSearch()
+  }, [searchIssues])
+
   return (
     <>
       <Profile>
-        <img src={userData.avatar_url} alt="User image" />
+        {userData ? (
+          <>
+            <img src={userData.avatar_url} alt="User image" />
 
-        <div className="profile-content">
-          <div className="profile-header">
-            <h1>{userData.name}</h1>
-            <div className="profile-link">
-              <a
-                href="https://github.com/casbern"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GITHUB
-                <ArrowSquareUpRight size={13} />
-              </a>
+            <div className="profile-content">
+              <div className="profile-header">
+                <h1>{userData.name}</h1>
+                <div className="profile-link">
+                  <a
+                    href="https://github.com/casbern"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    GITHUB
+                    <ArrowSquareUpRight size={13} />
+                  </a>
+                </div>
+              </div>
+
+              <p>{userData.bio}</p>
+
+              <div className="profile-info">
+                <div className="profile">
+                  <GithubLogo size={18} />
+                  <span>{userData.login}</span>
+                </div>
+
+                <div className="profile">
+                  <Buildings size={18} />
+                  <span>
+                    {userData.company ? userData.company : 'Não informado'}
+                  </span>
+                </div>
+
+                <div className="profile">
+                  <Users size={18} />
+                  <span>{userData.followers} seguidores</span>
+                </div>
+              </div>
             </div>
-          </div>
+          </>
+        ) : (
+          <>
+            <img src={avatar} alt="User image" />
 
-          <p>{userData.bio}</p>
+            <div className="profile-content">
+              <div className="profile-header">
+                <h1>Cameron Williamson</h1>
+                <div className="profile-link">
+                  <a
+                    href="https://github.com/casbern"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    GITHUB
+                    <ArrowSquareUpRight size={13} />
+                  </a>
+                </div>
+              </div>
 
-          <div className="profile-info">
-            <div className="profile">
-              <GithubLogo size={18} />
-              <span>{userData.login}</span>
+              <p>
+                Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+                viverra massa quam dignissim aenean malesuada suscipit. Nunc,
+                volutpat pulvinar vel mass.
+              </p>
+
+              <div className="profile-info">
+                <div className="profile">
+                  <GithubLogo size={18} />
+                  <span>cameronwll</span>
+                </div>
+
+                <div className="profile">
+                  <Buildings size={18} />
+                  <span>Rocketseat</span>
+                </div>
+
+                <div className="profile">
+                  <Users size={18} />
+                  <span>32 seguidores</span>
+                </div>
+              </div>
             </div>
-
-            <div className="profile">
-              <Buildings size={18} />
-              <span>Rocketseat</span>
-            </div>
-
-            <div className="profile">
-              <Users size={18} />
-              <span>{userData.followers} seguidores</span>
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </Profile>
 
       <Search>
@@ -74,89 +168,26 @@ export function Blog() {
           <span>6 publicações</span>
         </div>
 
-        <input type="text" placeholder="Buscar conteúdo" />
+        <input
+          type="text"
+          placeholder="Buscar conteúdo"
+          onChange={handleSearchIssues}
+          value={searchIssues}
+        />
       </Search>
 
       <Cards>
-        <Card>
-          <div className="card-header">
-            <h1>JavaScript data types and data structures</h1>
-            <span>Há 1 dia</span>
-          </div>
+        {searchResult &&
+          searchResult.map((result) => (
+            <Card key={result.id}>
+              <div className="card-header">
+                <h1>{result.title}</h1>
+                <span>{result.created_at}</span>
+              </div>
 
-          <p>
-            Programming languages all have built-in data structures, but these
-            often differ from one language to another. This article attempts to
-            list the built-in data structures available in JavaScript and what
-            properties they have. These can be used to build other data
-            structures. Wherever possible, comparisons with other languages are
-            drawn. Dynamic typing JavaScript is a loosely typed and dynamic
-            language. Variables in JavaScript are not directly associated with
-            any particular value type, and any variable can be assigned (and
-            re-assigned) values of all types: let foo = 42; // foo is now a
-            number foo // foo is now a string foo = true; // foo is now boolean
-          </p>
-        </Card>
-
-        <Card>
-          <div className="card-header">
-            <h1>JavaScript data types and data structures</h1>
-            <span>Há 1 dia</span>
-          </div>
-
-          <p>
-            Programming languages all have built-in data structures, but these
-            often differ from one language to another. This article attempts to
-            list the built-in data structures available in JavaScript and what
-            properties they have. These can be used to build other data
-            structures. Wherever possible, comparisons with other languages are
-            drawn. Dynamic typing JavaScript is a loosely typed and dynamic
-            language. Variables in JavaScript are not directly associated with
-            any particular value type, and any variable can be assigned (and
-            re-assigned) values of all types: let foo = 42; // foo is now a
-            number foo // foo is now a string foo = true; // foo is now boolean
-          </p>
-        </Card>
-
-        <Card>
-          <div className="card-header">
-            <h1>JavaScript data types and data structures</h1>
-            <span>Há 1 dia</span>
-          </div>
-
-          <p>
-            Programming languages all have built-in data structures, but these
-            often differ from one language to another. This article attempts to
-            list the built-in data structures available in JavaScript and what
-            properties they have. These can be used to build other data
-            structures. Wherever possible, comparisons with other languages are
-            drawn. Dynamic typing JavaScript is a loosely typed and dynamic
-            language. Variables in JavaScript are not directly associated with
-            any particular value type, and any variable can be assigned (and
-            re-assigned) values of all types: let foo = 42; // foo is now a
-            number foo // foo is now a string foo = true; // foo is now boolean
-          </p>
-        </Card>
-
-        <Card>
-          <div className="card-header">
-            <h1>JavaScript data types and data structures</h1>
-            <span>Há 1 dia</span>
-          </div>
-
-          <p>
-            Programming languages all have built-in data structures, but these
-            often differ from one language to another. This article attempts to
-            list the built-in data structures available in JavaScript and what
-            properties they have. These can be used to build other data
-            structures. Wherever possible, comparisons with other languages are
-            drawn. Dynamic typing JavaScript is a loosely typed and dynamic
-            language. Variables in JavaScript are not directly associated with
-            any particular value type, and any variable can be assigned (and
-            re-assigned) values of all types: let foo = 42; // foo is now a
-            number foo // foo is now a string foo = true; // foo is now boolean
-          </p>
-        </Card>
+              <p>{result.body}</p>
+            </Card>
+          ))}
       </Cards>
     </>
   )
